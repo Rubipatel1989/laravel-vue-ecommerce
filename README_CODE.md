115f33ca80026db9bcc24b65e3ca8e762d06f4d1
Pawan Kumar <pawankum0010@gmail.com>
2024-03-06 07:52:49 +0530

    Create Model

--- README_CODE.md
+++ README_CODE.md
@@ -3,3 +3,7 @@ Youtube link - https://www.youtube.com/watch?v=jffKw_NMfnw
 laravel-10-Vue
 composer global require laravel/installer
 laravel new laravel-vue-ecommerce
+php artisan serve
+php artisan make:model Product -m 
+php artisan make:model Order -m 
+Today 30.41
---
+++ database/migrations/2024_03_06_013317_create_order_details_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_details', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name',100);
+            $table->string('last_name',100);
+            $table->string('phone',15)->nullable();
+            $table->string('address1',100);
+            $table->string('address2',100);
+            $table->string('city',100);
+            $table->string('state',100)->nullable();
+            $table->integer('zipcode',6)->nullable();
+            $table->string('country_code',3);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_details');
+    }
+};
